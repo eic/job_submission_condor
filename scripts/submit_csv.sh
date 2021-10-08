47fc67faf2ca28c87d7888f2e936ae4f141c6a31
Wouter Deconinck <wdconinc@gmail.com>
2021-10-08 00:02:55 -0500

    Script to submit by csv from CI

---
+++ scripts/submit_csv.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# check arguments
+if [ $# -lt 2 ] ; then
+  echo "Usage: "
+  echo "  $0 <template> <type> <file or url> [target hours = 2]"
+  exit
+fi
+
+# Project configuration
+BASEURL="https://eicweb.phy.anl.gov/api/v4/projects/491/jobs/artifacts/master/raw/results/datasets/timings/"
+BASEJOB="?job=collect"
+
+# Parse arguments
+# - condor template
+TEMPLATE=${1}
+shift
+# - type of simulation
+TYPE=${1}
+shift
+# - input file
+FILE=${1}
+shift
+# - target hours
+TARGET=${1:-2}
+shift
+
+# Parse input URI
+if [ -f "${FILE}" ] ; then
+  INPUT="cat ${FILE}"
+else
+  INPUT="curl ${BASEURL}${FILE}${BASEJOB}"
+fi
+
+# Loop over input
+${INPUT} | grep -v ^curl | while IFS="," read file ntotal dt0 dt1 ; do
+  nevents=$(echo "(3600*$TARGET-$dt0)/$dt1" | bc)
+  nchunks=$(echo "($ntotal/$nevents)" | bc)
+  $(dirname $0)/submit.sh ${TEMPLATE} ${TYPE} ${file} ${nevents} ${nchunks}
+done
