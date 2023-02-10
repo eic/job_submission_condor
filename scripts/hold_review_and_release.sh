9c9d0d7184a089998d7710099384c028d9e3f55a
Wouter Deconinck <wouter.deconinck@umanitoba.ca>
2023-02-10 14:28:44 -0600

    feat: hold review: allow multiple jobids, robustness

--- scripts/hold_review_and_release.sh
+++ scripts/hold_review_and_release.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
 n=1
-N=$(condor_q ${1:-} -constraint 'JobStatus == 5' -af ClusterID ProcID | wc -l)
+N=$(condor_q ${*} -constraint 'JobStatus == 5' -af ClusterID ProcID | wc -l)
 
-condor_q ${1:-} -constraint 'JobStatus == 5' -af ClusterID ProcID NumJobStarts | while read ClusterID ProcID NumJobStarts ; do
+condor_q ${*} -constraint 'JobStatus == 5' -af ClusterID ProcID NumJobStarts | while read ClusterID ProcID NumJobStarts ; do
   echo "Job ${ClusterID}.${ProcID} ${NumJobStarts} ($n/$N)"
-  prefix="LOG/CONDOR/osg_${ClusterID}_${ProcID}"
+  if [[ ${ClusterID} > 26930000 ]] ; then
+    prefix="LOG/CONDOR/osg_${ClusterID}/osg_${ClusterID}_${ProcID}"
+  else
+    prefix="LOG/CONDOR/osg_${ClusterID}_${ProcID}"
+  fi
+
+  # Get logs from S3
+  mc cp S3/eictest/EPIC/${prefix}.err ${prefix}.err || true
+  mc cp S3/eictest/EPIC/${prefix}.out ${prefix}.out || true
 
   # Copy hold record
   for i in ${prefix}.* ; do
@@ -16,7 +24,13 @@ condor_q ${1:-} -constraint 'JobStatus == 5' -af ClusterID ProcID NumJobStarts |
 
   # Common errors for automatic release
   if test -f ${prefix}.err ; then
+    if grep "FATAL.*bad file descriptor" ${prefix}.err ; then
+      condor_release ${ClusterID}.${ProcID}
+      n=$((n+1))
+      continue
+    fi
     if grep "Transport endpoint is not connected" ${prefix}.err ; then
+      read <&1
       condor_release ${ClusterID}.${ProcID}
       n=$((n+1))
       continue
@@ -24,14 +38,21 @@ condor_q ${1:-} -constraint 'JobStatus == 5' -af ClusterID ProcID NumJobStarts |
     if grep "Unable to initialize new alias from the provided credentials." ${prefix}.err ; then
       grep ^resource ${prefix}.out
       grep ^hostname ${prefix}.out
-      grep -A20 tracepath ${prefix}.out | tee -a tracepath.log
+      grep -A20 tracepath ${prefix}.out
+      read <&1
       condor_release ${ClusterID}.${ProcID}
       n=$((n+1))
       continue
     fi
     if grep "tracepath: dtn01.sdcc.bnl.gov: Temporary failure in name resolution" ${prefix}.err ; then
       grep hostname ${prefix}.out
-      grep -A20 tracepath ${prefix}.out | tee -a tracepath.log
+      grep -A20 tracepath ${prefix}.out
+      read <&1
+      condor_release ${ClusterID}.${ProcID}
+      n=$((n+1))
+      continue
+    fi
+    if grep "mount /hadoop->/hadoop error" ${prefix}.err ; then
       read <&1
       condor_release ${ClusterID}.${ProcID}
       n=$((n+1))
@@ -50,17 +71,35 @@ condor_q ${1:-} -constraint 'JobStatus == 5' -af ClusterID ProcID NumJobStarts |
     #  continue
     #fi
     if grep -Pzo "Failed to load ID decoder for HcalBarrelHits" ${prefix}.out ; then
+      read <&1
       condor_release ${ClusterID}.${ProcID}
       n=$((n+1))
       continue
     fi
   fi
   if test -f ${prefix}.out ; then
+    if grep "GeomNav0003" ${prefix}.out && grep "lens_groove" ${prefix}.out ; then
+      condor_rm ${ClusterID}.${ProcID}
+      n=$((n+1))
+      continue
+    fi
+    if grep "Bus error" ${prefix}.out ; then
+      condor_release ${ClusterID}.${ProcID}
+      n=$((n+1))
+      continue
+    fi
+    if grep "^SysError.*No such file or directory$" ${prefix}.out ; then
+      grep ^resource ${prefix}.out
+      grep ^hostname ${prefix}.out
+      condor_release ${ClusterID}.${ProcID}
+      n=$((n+1))
+      continue
+    fi
     if grep "Unable to initialize new alias from the provided credentials." ${prefix}.out ; then
       grep ^date ${prefix}.out
       grep ^resource ${prefix}.out
       grep ^hostname ${prefix}.out
-      grep -A20 tracepath ${prefix}.out | tee -a tracepath.log
+      grep -A20 tracepath ${prefix}.out
       read <&1
       condor_release ${ClusterID}.${ProcID}
       n=$((n+1))
@@ -69,7 +108,7 @@ condor_q ${1:-} -constraint 'JobStatus == 5' -af ClusterID ProcID NumJobStarts |
     if grep "Unable to validate source" ${prefix}.out ; then
       grep ^resource ${prefix}.out
       grep ^hostname ${prefix}.out
-      grep -A20 tracepath ${prefix}.out | tee -a tracepath.log
+      grep -A20 tracepath ${prefix}.out
       read <&1
       condor_release ${ClusterID}.${ProcID}
       n=$((n+1))
@@ -78,7 +117,8 @@ condor_q ${1:-} -constraint 'JobStatus == 5' -af ClusterID ProcID NumJobStarts |
     if grep "No internet connection." ${prefix}.out ; then
       grep ^resource ${prefix}.out
       grep ^hostname ${prefix}.out
-      grep -A20 tracepath ${prefix}.out | tee -a tracepath.log
+      grep -A20 tracepath ${prefix}.out
+      read <&1
       condor_release ${ClusterID}.${ProcID}
       n=$((n+1))
       continue
@@ -99,15 +139,17 @@ condor_q ${1:-} -constraint 'JobStatus == 5' -af ClusterID ProcID NumJobStarts |
   fi
   if test -f ${prefix}.log ; then
     if grep "put on hold by SYSTEM_PERIODIC_HOLD due to memory usage" ${prefix}.log ; then
+      read <&1
       condor_rm ${ClusterID}.${ProcID}
       n=$((n+1))
       continue
     fi
-    if grep "memory usage exceeded request_memory" ${prefix}.log ; then
-      condor_rm ${ClusterID}.${ProcID}
-      n=$((n+1))
-      continue
-    fi
+    #if grep "memory usage exceeded request_memory" ${prefix}.log ; then
+    #  read <&1
+    #  condor_release ${ClusterID}.${ProcID}
+    #  n=$((n+1))
+    #  continue
+    #fi
   fi
 
   ls -al ${prefix}.*
