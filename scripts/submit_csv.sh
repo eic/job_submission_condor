3b06b0b7c3bf7fdff0e586e8861fd5219d385b4d
Sakib Rahman <rahmans@myumanitoba.ca>
2024-12-18 14:30:23 -0500

    Reverse order of input file basename and extension

--- scripts/submit_csv.sh
+++ scripts/submit_csv.sh
@@ -38,7 +38,7 @@ fi
 
 # create command line
 EXECUTABLE="$(dirname $0)/run.sh"
-ARGUMENTS="\$(ext) EVGEN/\$(file) \$(nevents) \$(ichunk)"
+ARGUMENTS="EVGEN/\$(file) \$(ext) \$(nevents) \$(ichunk)"
 
 # construct environment file
 ENVIRONMENT=environment-$(date --iso-8601=minutes).sh
