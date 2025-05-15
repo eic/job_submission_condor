509f8c5a8d171dc6c397d0814e9be5b15c6b64db
Sakib Rahman <rahmans@myumanitoba.ca>
2025-05-15 10:54:24 -0400

    Script to set background mixing frequency variables (#41)
    * Script to set background mixing frequency variables
    
    * Move to scripts directory
    
    * Fix downloading via curl and setting key value pairs
    
    * Use tagged version of the datasets repository. For now assuming, the tag will be same as the epic monthly tags unless we decide otherwise
    
    * Revert to using main branch of datasets repo to get config data
    
    Not ready to implement version tagged frequency data yet
    
    * Change the third background source to synchroton radiation
    
    * Added variables for background mixing in environment setting script
    
    * Added background mixing parameters in environment setting template
    
    * Execute the background mixing variable setting script in job submission
    
    * Rename set_bg_mix_freq.sh to set_bg_mix_var.sh
    
    * Expand environment setting script to accept file and skip variables alongside frequency
    
    * Fix typo: Replace minbias with synrad
    
    * Fix typo. Missing double quotes.
    
    * Add units to variable look up
    
    * Fix typo. EBEAM is the reference key.
    
    * Function to trim units
    
    * Change method to trim units and remove white space from variables
    
    * Add vacuum variable in environment setting template
    
    * Add vacuum variable in environment setting template in submission script

---
+++ scripts/set_bg_mix_var.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+set -Euo pipefail
+trap 's=$?; echo "$0: Error on line "$LINENO": $BASH_COMMAND"; exit $s' ERR
+IFS=$'\n\t'
+
+# Set url for frequency tables
+EGAS_URL=https://raw.githubusercontent.com/eic/simulation_campaign_datasets/main/config_data/egas_freq.csv
+HGAS_URL=https://raw.githubusercontent.com/eic/simulation_campaign_datasets/main/config_data/hgas_freq.csv
+SYNRAD_URL=https://raw.githubusercontent.com/eic/simulation_campaign_datasets/main/config_data/synrad_freq.csv
+
+# Download tables
+EGAS_TABLE=$( curl -L ${EGAS_URL} )
+HGAS_TABLE=$( curl -L ${HGAS_URL} )
+SYNRAD_TABLE=$( curl -L ${SYNRAD_URL} )
+
+# Initialize associative arrays (maps) for each type of background
+declare -A EGAS_FILE EGAS_FREQ EGAS_SKIP HGAS_FILE HGAS_FREQ HGAS_SKIP SYNRAD_FILE SYNRAD_FREQ SYNRAD_SKIP
+
+# Function to trim units
+trim() {
+  local input="$1"
+  local unit="$2"
+  # Remove unit
+  input="${input//${unit}/}"
+  echo "$input"
+}
+
+# Function to process each line into a map
+process_lines() {
+    local data="$1"
+    local -n map_file="$2"
+    local -n map_freq="$3"
+    local -n map_skip="$4"
+    local index=0
+
+    while IFS=',' read -r config file freq skip; do
+        # Adding the line to the map with a simple index as the key
+        map_file[$config]="$(echo $file | xargs)"
+        map_freq[$config]="$(echo $(trim $freq kHz) | xargs)"
+        map_skip[$config]="$(echo $skip | xargs)"
+    done <<< "$data"
+}
+
+# Process the downloaded data and populate the maps
+process_lines "$EGAS_TABLE" EGAS_FILE EGAS_FREQ EGAS_SKIP
+process_lines "$HGAS_TABLE" HGAS_FILE HGAS_FREQ HGAS_SKIP
+process_lines "$SYNRAD_TABLE" SYNRAD_FILE SYNRAD_FREQ SYNRAD_SKIP
+
+export BG1_FILE=${EGAS_FILE[${EBEAM}GeVx${PBEAM}GeV_${VAC}Ahr]}
+export BG1_FREQ=${EGAS_FREQ[${EBEAM}GeVx${PBEAM}GeV_${VAC}Ahr]}
+export BG1_SKIP=${EGAS_SKIP[${EBEAM}GeVx${PBEAM}GeV_${VAC}Ahr]}
+
+export BG2_FILE=${HGAS_FILE[${EBEAM}GeVx${PBEAM}GeV_${VAC}Ahr]}
+export BG2_FREQ=${HGAS_FREQ[${EBEAM}GeVx${PBEAM}GeV_${VAC}Ahr]}
+export BG2_SKIP=${HGAS_SKIP[${EBEAM}GeVx${PBEAM}GeV_${VAC}Ahr]}
+
+export BG3_FILE=${SYNRAD_FILE[${EBEAM}GeV]}
+export BG3_FREQ=${SYNRAD_FREQ[${EBEAM}GeV]}
+export BG3_SKIP=${SYNRAD_SKIP[${EBEAM}GeV]}
