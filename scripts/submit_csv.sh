25ca894c41fdef1706014e7b7cc7ce17c750dd87
Sakib Rahman <rahmans@myumanitoba.ca>
2025-05-28 12:46:11 -0400

    Prevent duplication of environment setting script naming in successive submissions (#49)

--- scripts/submit_csv.sh
+++ scripts/submit_csv.sh
@@ -44,7 +44,7 @@ ARGUMENTS="EVGEN/\$(file) \$(ext) \$(nevents) \$(ichunk)"
 source $(dirname $0)/set_bg_mix_var.sh
 
 # construct environment file
-ENVIRONMENT=environment-$(date --iso-8601=minutes).sh
+ENVIRONMENT=environment-$(date --iso-8601=ns).sh
 
 # extract certificate name
 X509_USER_PROXY_BASE=$(basename ${X509_USER_PROXY:-})
